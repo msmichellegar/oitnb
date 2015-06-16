21130afb737b4bf4b37733eae29a520943062ddb
msmichellegar <msmichellegar@gmail.com>
2015-06-16 02:33:56 +0100

    disable qunit

--- frontEndTest.js
+++ frontEndTest.js
@@ -1,12 +1,12 @@
-
-
-test("CSS is loading properly", function() {
-  var content = getComputedStyle(document.getElementById('content'), null).margin;
-  equal(content, '50px 100px', 'css is present');
-});
-
-test("toggleSlide() switches tabs correctly", function() {
-  toggleSlide(0);
-  clapContainer = getComputedStyle(document.getElementById('clapContainer'), null).display;
-  equal(clapContainer, 'none', 'tabs function correctly');
-});
+//
+//
+// test("CSS is loading properly", function() {
+//   var content = getComputedStyle(document.getElementById('content'), null).margin;
+//   equal(content, '50px 100px', 'css is present');
+// });
+//
+// test("toggleSlide() switches tabs correctly", function() {
+//   toggleSlide(0);
+//   clapContainer = getComputedStyle(document.getElementById('clapContainer'), null).display;
+//   equal(clapContainer, 'none', 'tabs function correctly');
+// });
